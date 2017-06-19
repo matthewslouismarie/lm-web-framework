994b520830b35f696f4d891ca191b95da9c3a3e4
Louis-Marie Matthews <louismarie.matthews@outlook.fr>
2017-06-19 19:19:54 +0200

    Display username in homepage

--- website/private/views/get_index.php
+++ website/private/views/get_index.php
@@ -4,6 +4,8 @@
         <title>Ça marche !</title>
     </head>
     <body>
+        <?php if (isConnected()) : ?>
         <h1>Hello, <?php echo getUsername() ?></h1>
+        <?php endif ?>
     </body>
 </html>
