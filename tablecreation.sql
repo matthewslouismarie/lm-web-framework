105fa3ea7a78885197b01fdfdc6d93324dc536d3
Louis-Marie Matthews <louismarie.matthews@outlook.fr>
2017-06-19 18:42:21 +0200

    Allow users to login

--- tablecreation.sql
+++ tablecreation.sql
@@ -3,7 +3,7 @@ CREATE TABLE person (
     firstname CHAR(255) NOT NULL,
     lastname CHAR(255) NOT NULL,
     birthdate DATE NOT NULL,
-    password VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL, # TODO: should be replaced by passwordseed and passwordcipher
     email CHAR(255) NOT NULL
     )
     ENGINE InnoDB,
@@ -13,7 +13,7 @@ CREATE TABLE person (
 CREATE TABLE run (
         id MEDIUMINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
         runnerid TINYINT UNSIGNED NOT NULL,
-        starttimestamp TIMESTAMP(0) NOT NULL,
+        starttimestamp TIMESTAMP(0) NOT NULL, # TODO: type should be updated in 2038
         durationseconds MEDIUMINT UNSIGNED NOT NULL,
         distancemeters MEDIUMINT UNSIGNED NOT NULL,
         FOREIGN KEY (runnerid) REFERENCES person (id) ON DELETE CASCADE ON UPDATE CASCADE
---
+++ website/private/views/get_index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Ça marche !</title>
+    </head>
+    <body>
+        <h1>Hello, <?php echo getUsername() ?></h1>
+    </body>
+</html>
