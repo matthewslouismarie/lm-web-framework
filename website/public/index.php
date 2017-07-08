83fb21cb6a06234fe2d44cbd1606a75e0fdd2b19
Louis-Marie Matthews <louismarie.matthews@outlook.fr>
2017-07-08 21:28:30 +0200

    Remove functions

--- website/public/index.php
+++ website/public/index.php
@@ -2,17 +2,17 @@
 
 // TODO: php doc
 // TODO: namespaces for autoLoadClass?
-// TODO: good html, semantics, accessibility WAI
+// TODO: good html, semantics, AMP, open graph, schema, accessibility WAI
 
 // Constants definition
 define('PDM_PAGE', 'page');
 define('PDM_SRC', dirname(__DIR__).'/private/');
 define('PDM_CLASSES', dirname(__DIR__).'/private/classes/');
-define('PDM_FUNCTIONS', dirname(__DIR__).'/private/functions/');
 
 // Autoloader's registration
-require_once(PDM_FUNCTIONS . 'autoLoadClass.php');
-spl_autoload_register( autoLoadClass );
+require_once(PDM_CLASSES . 'LM/Autoloader/PhpFigAutoloader.php');
+$autoloader = new LM\Autoloader\PhpFigAutoloader;
+spl_autoload_register(array($autoloader, 'autoLoadClass'));
 
 // Request processing
 $router = new LM\WebFramework\Router;
