c904accd0ec5fae17f86af00b2a75ced7b52c0c3
Louis-Marie Matthews <louismarie.matthews@outlook.fr>
2017-07-25 02:36:14 +0200

    Display custom greetings for logged-in users

--- website/private/classes/LM/PersonalDataManager/View/HomeView.php
+++ website/private/classes/LM/PersonalDataManager/View/HomeView.php
@@ -2,15 +2,19 @@
 
 namespace LM\PersonalDataManager\View;
 
+use LM\WebFramework\Identification\UserIdentification;
+
 class HomeView extends TemplateView
 {
 	private $breadcrumb;
+	private $userIdentification;
 
 	public function __construct()
 	{
 		$this->breadcrumb = array(
 			'Home' => 'index.php',
 		);
+		$this->userIdentification = new UserIdentification;
 	}
 	public function display(): void
 	{
@@ -22,6 +26,9 @@ class HomeView extends TemplateView
 	</head>
 	<body>
 		<?php $this->displayPageHeader('Login', $this->breadcrumb) ?>
+		<?php if ($this->userIdentification->isLoggedIn()) : ?>
+		<aside class="first-level-container"><p>Hello, <?= $this->userIdentification->getUsername()->getString() ?>.</p></aside>
+		<?php endif ?>
 		<main>
 			<div class="main-content-container">
 				<p>Personal Data Manager is the essential tool to help you manage your life! <a href="index.php?page=login">Log in!</a></p>
