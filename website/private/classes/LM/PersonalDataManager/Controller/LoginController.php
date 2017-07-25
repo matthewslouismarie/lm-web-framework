794cbbd098daec1ae93516e08f60e7ec8ed60cb0
Louis-Marie Matthews <louismarie.matthews@outlook.fr>
2017-07-25 02:54:41 +0200

    Prevent access to login page to logged-in members

--- website/private/classes/LM/PersonalDataManager/Controller/LoginController.php
+++ website/private/classes/LM/PersonalDataManager/Controller/LoginController.php
@@ -11,37 +11,52 @@ use LM\WebFramework\Form\FormReader;
 
 class LoginController implements IPageController
 {
-	public function
+	private $userIdentification;
+
+	public function __construct()
+	{
+		$this->userIdentification = new UserIdentification;
+	}
+
 	public function doGet(): void
 	{
-		$view = new LoginView(new FormReader(array()));
-		$view->display();
+		if ($this->userIdentification->isLoggedIn()) {
+			$view = new ErrorNotice('You cannot access the login page while being logged in.', 'Access Denied');
+			$view->display();
+		} else {
+			$view = new LoginView(new FormReader(array()));
+			$view->display();
+		}
 	}
 
 	public function doPost(array $postData): void
 	{
-		$formReader = new FormReader($postData);
-		$formReader->readUsername('username');
-		$formReader->readPassword('password');
-
-		if ($formReader->hasErrors()) {
-			$view = new LoginView($formReader);
+		if ($this->userIdentification->isLoggedIn()) {
+			$view = new ErrorNotice('You cannot access the login page while being logged in.', 'Access Denied');
 			$view->display();
 		} else {
-			$pdo = DatabaseConnection::getInstance()->getPdo();
-			// TODO: should it check that the user is not already logged in?
-			$query = 'SELECT COUNT(id) FROM `shift-two_pdm`.person WHERE id = :username AND password = :password';
-			$request = $pdo->prepare($query);
-			$request->execute(array('username' => $formReader->getUsername('username')->getString(), 'password' => $formReader->getPassword('password')->getString()));
-			if ("1" === $request->fetchAll()[0]["COUNT(id)"]) {
-				$userIdentification = new UserIdentification;
-				$userIdentification->logUserIn($formReader->getUsername('username'));
-				$view = new SuccessNotice('You logged in successfully', 'Successful Login');
-				$view->display();
-			} else {
-				$formReader->addGeneralError('Incorrect Credentials.');
+			$formReader = new FormReader($postData);
+			$formReader->readUsername('username');
+			$formReader->readPassword('password');
+
+			if ($formReader->hasErrors()) {
 				$view = new LoginView($formReader);
 				$view->display();
+			} else {
+				$pdo = DatabaseConnection::getInstance()->getPdo();
+				// TODO: should it check that the user is not already logged in?
+				$query = 'SELECT COUNT(id) FROM `shift-two_pdm`.person WHERE id = :username AND password = :password';
+				$request = $pdo->prepare($query);
+				$request->execute(array('username' => $formReader->getUsername('username')->getString(), 'password' => $formReader->getPassword('password')->getString()));
+				if ("1" === $request->fetchAll()[0]["COUNT(id)"]) {
+					$this->userIdentification->logUserIn($formReader->getUsername('username'));
+					$view = new SuccessNotice('You logged in successfully', 'Successful Login');
+					$view->display();
+				} else {
+					$formReader->addGeneralError('Incorrect Credentials.');
+					$view = new LoginView($formReader);
+					$view->display();
+				}
 			}
 		}
 	}
