83f34d5ec01a1fa352d0ef361897395d2d9dc5c9
Louis-Marie Matthews <louismarie.matthews@outlook.fr>
2017-11-19 15:03:57 +0000

    Remove obsolete, useless or irrelevant comments

--- classes/LM/WebFramework/Controller/MainController.php
+++ classes/LM/WebFramework/Controller/MainController.php
@@ -14,16 +14,7 @@ class MainController
         if (false === $is_session_started) {
             throw new SessionStartFailureException();
         }
-
-        // TODO: php doc
-        // TODO: namespaces for autoLoadClass?
-        // TODO: good html, semantics, AMP, open graph, schema, accessibility WAI
-
-        // Request processing
-        // TODO: maybe the main controller could get these things, such as the project
-        // specific router, from an interface implemented by the project? Quite useless
-        // for now as the main controller only needs the router. But there are constants
-        // to define.
+        
         $controller = $router->getControllerFromRequest();
 
         if ($_SERVER['REQUEST_METHOD'] === 'GET') {
