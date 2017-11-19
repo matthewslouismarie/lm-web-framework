29943834bdd710ad21a4959b65cde67007ddb625
Louis-Marie Matthews <louismarie.matthews@outlook.fr>
2017-11-19 14:01:00 +0000

    Reorganise files and delete public folder

---
+++ classes/LM/WebFramework/Controller/MainController.php
@@ -0,0 +1,42 @@
+<?php
+
+class MainController
+{
+    public function processRequest()
+    {
+        session_start();
+
+        // TODO: php doc
+        // TODO: namespaces for autoLoadClass?
+        // TODO: good html, semantics, AMP, open graph, schema, accessibility WAI
+
+        // Constants definition
+        // These constants should be removed? The main controller should not contain
+        // project-related code.
+        define('PDM_PAGE', 'page');
+        define('PDM_SRC', dirname(__DIR__).'/private/');
+        define('PDM_CLASSES', dirname(__DIR__).'/private/classes/');
+        define('PDM_USERNAME_MAX_LENGTH', 255);
+        define('PDM_PASSWORD_MAX_LENGTH', 255);
+        define('PDM_MAX_LENGTH', 255);
+
+        // Autoloader's registration
+        require_once(PDM_CLASSES . 'LM/Autoloader/PhpFigAutoloader.php');
+        $autoloader = new LM\Autoloader\PhpFigAutoloader;
+        spl_autoload_register(array($autoloader, 'autoLoadClass'));
+
+        // Request processing
+        // TODO: maybe the main controller could get these things, such as the project
+        // specific router, from an interface implemented by the project? Quite useless
+        // for now as the main controller only needs the router. But there are constants
+        // to define.
+        $router = new LM\PersonalDataManager\Routing\Router();
+        $controller = $router->getControllerFromRequest();
+
+        if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+            $controller->doGet();
+        } elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $controller->doPost($_POST);
+        }
+    }
+}
