98bb5850e259dd037eb9a04084583334e8383524
Louis-Marie Matthews <louismarie.matthews@outlook.fr>
2017-11-19 15:02:56 +0000

    Turn $router into a parameter

--- classes/LM/WebFramework/Controller/MainController.php
+++ classes/LM/WebFramework/Controller/MainController.php
@@ -3,10 +3,11 @@
 namespace LM\WebFramework\Controller;
 
 use LM\Exception\SessionStartFailureException;
+use LM\PersonalDataManager\Routing\Router;
 
 class MainController
 {
-    public function processRequest()
+    public function processRequest(Router $router)
     {
         $is_session_started = session_start();
 
@@ -23,7 +24,6 @@ class MainController
         // specific router, from an interface implemented by the project? Quite useless
         // for now as the main controller only needs the router. But there are constants
         // to define.
-        $router = new LM\PersonalDataManager\Routing\Router();
         $controller = $router->getControllerFromRequest();
 
         if ($_SERVER['REQUEST_METHOD'] === 'GET') {
