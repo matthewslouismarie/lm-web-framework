0afba5c88cc4d4692b95da69fbe3280b3524bf79
Louis-Marie Matthews <louismarie.matthews@outlook.fr>
2017-11-19 15:01:58 +0000

    Remove PDM constants

--- classes/LM/WebFramework/Controller/MainController.php
+++ classes/LM/WebFramework/Controller/MainController.php
@@ -18,16 +18,6 @@ class MainController
         // TODO: namespaces for autoLoadClass?
         // TODO: good html, semantics, AMP, open graph, schema, accessibility WAI
 
-        // Constants definition
-        // These constants should be removed? The main controller should not contain
-        // project-related code.
-        define('PDM_PAGE', 'page');
-        define('PDM_SRC', dirname(__DIR__).'/private/');
-        define('PDM_CLASSES', dirname(__DIR__).'/private/classes/');
-        define('PDM_USERNAME_MAX_LENGTH', 255);
-        define('PDM_PASSWORD_MAX_LENGTH', 255);
-        define('PDM_MAX_LENGTH', 255);
-
         // Request processing
         // TODO: maybe the main controller could get these things, such as the project
         // specific router, from an interface implemented by the project? Quite useless
