5bdc0cb9e64ff5eb1ef0ed6ec23002aaf37e8d4b
Louis-Marie Matthews <17742458+matthewslouismarie@users.noreply.github.com>
2024-01-07 18:32:34 +0100

    Convert errors and warnings to exceptions

--- src/Kernel.php
+++ src/Kernel.php
@@ -5,12 +5,18 @@ namespace LM\WebFramework;
 use DI\ContainerBuilder;
 use LM\WebFramework\Http\HttpRequestHandler;
 use Psr\Container\ContainerInterface;
+use RuntimeException;
 
 class Kernel
 {
     const CLI_ID = 'cli';
 
     public static function initialize(string $projectRootPath): ?ContainerInterface {
+        set_error_handler(function ($errNo, $errStr, $errFile, $errLine) {
+            $msg = "$errNo, $errStr in $errFile on line $errLine";
+            throw new RuntimeException($msg, $errNo);
+        });
+    
         $container = (new ContainerBuilder())
             ->addDefinitions([
                 Configuration::class => new Configuration($projectRootPath),
