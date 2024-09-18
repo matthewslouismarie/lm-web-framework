ca381f1cd4ef45a89a5806ae89f80f0a827b65d2
Louis-Marie Matthews <17742458+matthewslouismarie@users.noreply.github.com>
2024-09-18 17:26:29 +0200

    Add RangeConstraint to ListModel

--- src/Model/Type/ListModel.php
+++ src/Model/Type/ListModel.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace LM\WebFramework\Model\Type;
 
+use LM\WebFramework\Model\Constraints\IRangeConstraint;
 use LM\WebFramework\Model\Type\AbstractModel;
 
 /**
@@ -12,7 +13,8 @@ use LM\WebFramework\Model\Type\AbstractModel;
 final class ListModel extends AbstractModel
 {
     public function __construct(
-        private IScalarModel|EntityModel|ListModel $itemModel,
+        private IScalarModel $itemModel,
+        private ?IRangeConstraint $rangeConstraint,
         bool $isNullable = false,
     ) {
         parent::__construct($isNullable);
@@ -21,5 +23,10 @@ final class ListModel extends AbstractModel
     public function getItemModel() : IScalarModel|EntityModel|ListModel
     {
         return $this->itemModel;
+    }
+
+    public function getRangeConstraint(): ?IRangeConstraint
+    {
+        return $this->rangeConstraint;
     }
 }
