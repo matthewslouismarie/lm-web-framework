7dbe139ce68fd2c6ff203371ee475123794b189c
Louis-Marie Matthews <matthewslouismarie@gmail.com>
2024-12-30 13:58:27 +0000

    Remove cache file from repo

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,10 @@
+<?php
+
+$finder = (new PhpCsFixer\Finder())
+    ->in(__DIR__)
+;
+
+return (new PhpCsFixer\Config())
+    ->setParallelConfig(PhpCsFixer\Runner\Parallel\ParallelConfigFactory::detect())
+    ->setFinder($finder)
+;
