0cf69e6b048a8549a40e06123e7880bc3866f18c
Louis-Marie Matthews <matthewslouismarie@gmail.com>
2024-12-30 12:28:06 +0000

    Fix a few PHP Stan errors

--- src/Session/SessionManager.php
+++ src/Session/SessionManager.php
@@ -29,19 +29,23 @@ final class SessionManager
     /**
      * @todo Should not accept null.
      */
-    public function setCurrentMemberUsername(?string $username): void {
+    public function setCurrentMemberUsername(?string $username): void
+    {
         $_SESSION[self::CURRENT_MEMBER_USERNAME] = $username;
     }
 
-    public function getCsrf(): string {
+    public function getCsrf(): string
+    {
         return $_SESSION[self::CSRF] ?? $_SESSION[self::CSRF] = bin2hex(random_bytes(self::CSRF_N_BYTES));
     }
 
-    public function addMessage(string $message): void {
+    public function addMessage(string $message): void
+    {
         isset($_SESSION[self::MESSAGES]) ? $_SESSION[self::MESSAGES][] = $message : $_SESSION[self::MESSAGES] = [$message];
     }
 
-    public function getAndDeleteMessages(): array {
+    public function getAndDeleteMessages(): array
+    {
         $messages = $_SESSION[self::MESSAGES] ?? [];
         $_SESSION[self::MESSAGES] = [];
         return $messages;
