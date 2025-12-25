9f837f74942c9f8bacdc30e7dfe14937309bf810
Louis-Marie Matthews <17742458+matthewslouismarie@users.noreply.github.com>
2025-12-25 21:24:26 +0000

    By default, do not authorized sub-routes to add roles

--- src/Http/Routing/RouteParser.php
+++ src/Http/Routing/RouteParser.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace LM\WebFramework\Http\Routing;
 
 use LM\WebFramework\Http\Routing\Exception\InvalidRouteConfException;
+use LM\WebFramework\Http\Routing\Exception\SubRouteCannotAddRoleConfException;
 use LM\WebFramework\Http\Routing\Exception\UnauthorizedAttributeConfException;
 
 final readonly class RouteParser
@@ -12,14 +13,15 @@ final readonly class RouteParser
     public function parseJson(string $filename): Route
     {
         $json_decoded = json_decode(file_get_contents($filename), associative: true, flags: JSON_THROW_ON_ERROR)["rootRoute"];
-        return $this->parse($json_decoded);
+        return $this->parse($json_decoded, allowOverridingParentRoles: true);
     }
 
     /**
      * @param array<string, mixed> $route The JSON-decoded route as an associative array.
      * @param string[] $parentRoles The roles of the parent route, if any.
+     * @param bool $allowOverridingParentRoles If true, a sub-route can add role its parent does not have.
      */
-    public function parse(array $route, array $parentRoles = []): Route
+    public function parse(array $route, array $parentRoles = [], bool $allowOverridingParentRoles = false): Route
     {
         foreach ($route as $key => $_) {
             if (!in_array($key, ['roles', 'controller', 'minArgs', 'maxArgs', 'routes'])) {
@@ -27,6 +29,11 @@ final readonly class RouteParser
             }
         }
         $roles = $route['roles'] ?? $parentRoles;
+        foreach ($roles as $role) {
+            if (!in_array($role, $parentRoles, strict: true) && !$allowOverridingParentRoles) {
+                throw new SubRouteCannotAddRoleConfException("Unless explicitely authorized, a sub-route cannot add roles its parent does not have.");
+            }
+        }
         $controller = str_replace('.', '\\', $route['controller']);
         if (key_exists('minArgs', $route) || key_exists('maxArgs', $route)) {
             if (key_exists('routes', $route)) {
