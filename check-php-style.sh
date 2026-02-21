c539968b121e007beaa8b8b04de72e8ca35d8c04
Louis-Marie Matthews <17742458+matthewslouismarie@users.noreply.github.com>
2026-02-21 15:23:41 +0200

    Remove "-o pipefail" option which does not exist in Shell

--- check-php-style.sh
+++ check-php-style.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-set -euo pipefail
+set -eu
 
 phpcs -qn --runtime-set ignore_warnings_on_exit 1 src
 phpcs -qn --exclude=PSR1.Classes.ClassDeclaration --runtime-set ignore_warnings_on_exit 1 tests
