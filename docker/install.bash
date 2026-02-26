54371dbb21119787a89d060912406cc662f47e88
Louis-Marie Matthews <17742458+matthewslouismarie@users.noreply.github.com>
2026-02-26 15:44:51 +0200

    Fix script to call Composer in container

--- docker/install.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-SDIR=$(dirname ${BASH_SOURCE[0]})/..
-docker compose cp $SDIR/composer.json composer_dsn:/app
-docker compose cp $SDIR/composer.lock composer_dsn:/app
-docker compose exec -it composer_dsn composer install --ignore-platform-reqs
