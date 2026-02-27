bf70bee6223fae5b9ce0dddec59c0347673e27d7
Louis-Marie Matthews <17742458+matthewslouismarie@users.noreply.github.com>
2026-02-27 11:31:58 +0000

    Remove "-it" flag from call to Composer service in composer.sh

--- docker/composer.sh
+++ docker/composer.sh
@@ -5,6 +5,6 @@ set -euo pipefail
 SDIR=$(dirname ${BASH_SOURCE[0]})/..
 docker compose cp $SDIR/composer.json composer_dsn:/app
 docker compose cp $SDIR/composer.lock composer_dsn:/app
-docker compose exec -it composer_dsn composer $@ --ignore-platform-reqs
+docker compose exec composer_dsn composer $@ --ignore-platform-reqs
 docker compose cp composer_dsn:/app/composer.json $SDIR
 docker compose cp composer_dsn:/app/composer.lock $SDIR
