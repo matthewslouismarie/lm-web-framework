e9226e1c74c20e0b83b709f0a7e7a82a77d92a95
Louis-Marie Matthews <17742458+matthewslouismarie@users.noreply.github.com>
2026-07-16 18:00:35 +0200

    Fix URL regex

--- src/Model/Factory/UrlModelFactory.php
+++ src/Model/Factory/UrlModelFactory.php
@@ -12,10 +12,7 @@ class UrlModelFactory
 
     public const URL_MIN_LENGTH = 1;
 
-    /**
-     * @link https://stackoverflow.com/questions/3809401/what-is-a-good-regular-expression-to-match-a-url
-     */
-    public const URL_REGEX = 'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)';
+    public const URL_REGEX = '^(https?:\/\/)?\w([\w.-]*\w)?\.\w+(\/.*)?';
 
     public function getUrlModel(bool $isNullable = false): StringModel
     {
